a872e0d127606bd1ebf3670dd8bdcf86d52049d8
Dvandenderen <danielvden@hotmail.com>
2026-02-26 13:49:10 +0100

    rename

---
+++ data/Maps_tables/Maps.R
@@ -0,0 +1,89 @@
+
+# set directory
+setwd("C:/Users/danie/OneDrive - Danmarks Tekniske Universitet/ICES_datacall/")
+
+# Load library
+library(readxl)
+library(dplyr)
+
+#-------------------------------------------------------------------------------
+# Get all Excel file names for type 2
+files <- list.files(path = "Type2/", pattern = "\\.xlsx$", full.names = TRUE)
+
+# Read sheet 2 from each file and combine
+all_data <- lapply(files, function(file) {
+  # Read sheet 2
+  df <- read_excel(file, sheet = 2)
+  
+  # Select longitude and latitude
+  df <- df %>%
+    select(longitude, latitude)
+  
+  # Add dataset column using the file name
+  df$type_2 <- basename(file)
+  
+  return(df)
+}) %>%
+  bind_rows()  # Combine all data frames into one
+
+# View combined dataset
+head(all_data)
+
+all_data$type <- "B. trawling (n=14)"
+all_data$type <- ifelse(all_data$type_2 %in% c("NS_Oostdyck_sandextr_and_btrawling.xlsx",
+                                               "NS_Thornton_sandextr_and_btrawling.xlsx",
+                                               "NS_Hinderbanken_sandextr_and_btrawling.xlsx"),
+                        "B. trawling + sand extr. (n=3)",
+                        all_data$type)
+
+all_data$type <- ifelse(all_data$type_2 %in% c("BS_southernbaltic_oxygendepletion.xlsx"),
+                        "Oygen depl. (n=1)",
+                        all_data$type)
+
+
+# make a map
+ctrys <- rnaturalearth::ne_countries(scale = 50, returnclass = "sf")
+
+type2 <- ggplot()+geom_point(data=all_data,aes(x=longitude,y=latitude,col=type),cex=1) +
+  geom_sf(data = ctrys, fill="lightgrey",colour= NA) + 
+  coord_sf(xlim = c(-15,33),ylim=c(34,67)) + theme_classic()
+
+ggsave("quick_plots/type_2.png", plot = type2, width = 8, height = 6, dpi = 300)
+
+#-------------------------------------------------------------------------------
+# Get all Excel file names for type 3
+files <- list.files(path = "Type3/", pattern = "\\.xlsx$", full.names = TRUE)
+
+# Read sheet 2 from each file and combine
+all_data <- lapply(files, function(file) {
+  # Read sheet 2
+  df <- read_excel(file, sheet = 2)
+  
+  # Select longitude and latitude (adjust names if needed)
+  df <- df %>%
+    select(longitude, latitude) %>%            # select relevant columns
+    mutate(
+      longitude = as.numeric(longitude),       # ensure numeric type
+      latitude  = as.numeric(latitude)
+    )
+  
+  # Add dataset column using the file name
+  df$type_3 <- basename(file)
+  
+  return(df)
+}) %>%
+  bind_rows()  # Combine all data frames into one
+
+# View combined dataset
+head(all_data)
+
+# make a map
+ctrys <- rnaturalearth::ne_countries(scale = 50, returnclass = "sf")
+
+type3 <- ggplot()+geom_point(data=all_data,aes(x=longitude,y=latitude,col=type_3)) +
+  geom_sf(data = ctrys, fill="lightgrey",colour= NA) + 
+  coord_sf(xlim = c(-15,33),ylim=c(34,67)) + theme_classic()
+
+ggsave("quick_plots/type_3.png", plot = type3, width = 8, height = 6, dpi = 300)
+
+
